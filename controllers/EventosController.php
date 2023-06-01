9ba67db3c07d4f82db28bd4282c7f990b8e329e2
Claudio Martel <claudiomarflo27@gmail.com>
2023-06-02 00:49:38 +0100

    formulario radio-button y DB

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Model\Categoria;
+use Model\Dia;
 use MVC\Router;
 
 class EventosController {
@@ -14,12 +15,14 @@ class EventosController {
     }
     public static function crear(Router $router){
         $alertas = [];
-        $categorias = Categoria::all();
+        $categorias = Categoria::all('ASC');
+        $dias = Dia::all('ASC');
 
         $router->render('admin/eventos/crear', [
             'titulo' => 'Registrar Evento',
             'alertas' => $alertas,
-            'categorias' => $categorias
+            'categorias' => $categorias,
+            'dias' => $dias
         ]);
     }
 }
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -23,4 +23,20 @@
         </select>
     </div>
 
+    <div class="formulario__campo">
+        <label for="categoria" class="formulario__label">Selecciona el Día</label>
+
+        <div class="formulario__radio">
+            <?php foreach($dias as $dia) { ?>
+                <div>
+                    <label for=<?php echo strtolower($dia->nombre);?>"><?php echo $dia->nombre;?></label>
+                    <input type="radio"
+                    id="<?php echo strtolower($dia->id);?>"
+                    name="dia"
+                    value="<?php echo $dia->id;?>">
+                </div>
+            <?php } ?>
+        </div>    
+    </div>
+
 </fieldset>
